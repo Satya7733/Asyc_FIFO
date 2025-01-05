650d507b3615e632d2e4c6a4950d0378fd977d54
Siddesh Dinesh Patil <patilsid@mo.ece.pdx.edu>
2025-01-05 12:32:20 -0800

    Adding FIFO_write module

--- FIFO_write.sv
+++ FIFO_write.sv
@@ -1,4 +1,4 @@
-`include "FIFO_mem.sv"
+`include "pkg_graybin.svh"
 
 module FIFO_write(
 input logic wren, wclk, wrst,
@@ -8,27 +8,6 @@ output logic [$clog2(DEPTH)-1:0]waddr,
 output logic wr_full);
 
 logic [$clog2(DEPTH):0] wrptr_t;
-
-    function automatic logic [$clog2(DEPTH):0] g2b (input logic [$clog2(DEPTH):0] gray);
-        logic [$clog2(DEPTH):0] binary;
-        integer i;
-        binary = 0;
-        for (i = $clog2(DEPTH); i >= 0; i--) begin
-            binary[i] = ^(gray >> i);
-        end
-        return binary;
-    endfunction
-
-    // Function to convert binary to Gray code
-    function automatic logic [$clog2(DEPTH):0] b2g (input logic [$clog2(DEPTH):0] binary);
-        logic [$clog2(DEPTH):0] gray;
-        integer i;
-        for (i = 0; i < $clog2(DEPTH); i++) begin
-            gray[i] = binary[i] ^ binary[i + 1];
-        end
-        gray[$clog2(DEPTH)] = binary[$clog2(DEPTH)];
-        return gray;
-    endfunction
 
 always @(posedge wclk or posedge wrst) begin
 	if (wrst) begin
