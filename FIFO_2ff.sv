34218433e18a1df8d60fe47b6e2ec776e75c827a
Siddesh Dinesh Patil <patilsid@mo.ece.pdx.edu>
2025-01-05 01:11:53 -0800

    Added synchromizer module

---
+++ FIFO_2ff.sv
@@ -0,0 +1,20 @@
+`include "FIFO_mem.sv"
+
+module FIFO_2ff #(parameter int DEPTH = 8) (
+input logic [0:$clog2(DEPTH)] ptr,
+input logic clk, rst,
+output logic [0:$clog2(DEPTH)] q2_ptr);
+
+logic [0:$clog2(DEPTH)] q1;
+
+always @(posedge clk or posedge rst) begin
+	if(rst) begin
+		q1 <= 0;
+		q2_ptr <= 0;
+	end else begin
+		q1 <= ptr;
+		q2_ptr <= q1;
+	end
+end
+endmodule
+	
