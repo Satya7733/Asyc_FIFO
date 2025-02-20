35891811cf50f32d23d4be484162eb80ee2af901
Satyajit Deokar <satyajit@ds.cecs.pdx.edu>
2025-02-20 02:23:32 -0800

    cleaned directories

--- Verification/AFIFO_Pkg.sv
+++
@@ -1,8 +0,0 @@
-package AFIFO_Pkg;
-  `include "AFIFO_Transaction.sv"
-  `include "AFIFO_Generator.sv"
-  `include "AFIFO_Driver.sv"
-  `include "AFIFO_Monitor.sv"
-  `include "AFIFO_Scoreboard.sv"
-  `include "AFIFO_Environment.sv"
-endpackage
