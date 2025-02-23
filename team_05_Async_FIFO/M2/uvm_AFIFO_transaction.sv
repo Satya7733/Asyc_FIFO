4e22bb9193afa88a296ed64c7e6d031d86bc68aa
Siddesh Dinesh Patil <patilsid@ds.cecs.pdx.edu>
2025-02-22 23:14:43 -0800

    git ignore 2

---
+++ team_05_Async_FIFO/M2/uvm_AFIFO_transaction.sv
@@ -0,0 +1,32 @@
+class uvm_AFIFO_transaction #(parameter DSIZE = 8) extends uvm_sequence_item;
+	 randc logic [DSIZE-1:0] wr_data;
+	 bit wr_clk, wr_rst, wr_inc;
+	 bit rd_clk, rd_rst, rd_inc;
+	 logic [DSIZE-1: 0]rd_data;
+	 logic [DSIZE-1: 0]rd_data_refModule;
+	 bit rd_empty,wr_full;
+
+	function AFIFO_Transaction copy();// create deep copy
+	copy = new();
+	copy.wr_data = this.wr_data;
+	copy.rd_data = this.rd_data;
+	copy.rd_empty = this.rd_empty;
+	copy.wr_full = this.wr_full;
+	copy.rd_data_refModule = this.rd_data_refModule;
+	 return copy;
+	endfunction
+
+	`uvm_object_utils_begin(uvm_AFIFO_transaction)
+	    `uvm_field_int(wr_data, UVM_ALL_ON)
+	    `uvm_field_int(rd_data, UVM_ALL_ON)
+	    `uvm_field_int(rd_data_refModule, UVM_ALL_ON)
+	    `uvm_field_int(rd_empty, UVM_ALL_ON)
+	    `uvm_field_int(wr_full, UVM_ALL_ON)
+	    `uvm_field_int(wr_clk, UVM_ALL_ON)
+	    `uvm_field_int(wr_rst, UVM_ALL_ON)
+	    `uvm_field_int(wr_inc, UVM_ALL_ON)
+	    `uvm_field_int(rd_clk, UVM_ALL_ON)
+	    `uvm_field_int(rd_rst, UVM_ALL_ON)
+	    `uvm_field_int(rd_inc, UVM_ALL_ON)
+	`uvm_object_utils_end
+endclass : uvm_AFIFO_transaction
