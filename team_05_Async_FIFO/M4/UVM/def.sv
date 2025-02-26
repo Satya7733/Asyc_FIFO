2848061feb5136fb9ed8d3ad58881689dd028443
Siddesh Dinesh Patil <patilsid@ds.cecs.pdx.edu>
2025-02-26 00:17:23 -0800

    rearraged directories

--- team_05_Async_FIFO/M4/UVM/def.sv
+++
@@ -1,7 +0,0 @@
-//  `include "FIFO.sv"
-//  `include "uvm_AFIFO_sequence_item.sv"
-//  `include "uvm_AFIFO_sequence.sv"
-//  `include "uvm_AFIFO_driver.sv"
-//  `include "uvm_AFIFO_monitor.sv"
-//  `include "uvm_AFIFO_scoreboard.sv"
-//
