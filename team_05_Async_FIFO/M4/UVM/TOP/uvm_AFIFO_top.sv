d97705404ca1b6a3a99c36fa088e053c5cdb24fd
Siddesh Dinesh Patil <patilsid@mo.ece.pdx.edu>
2025-02-26 18:05:51 -0800

    Added top and intreface

---
+++ team_05_Async_FIFO/M4/UVM/TOP/uvm_AFIFO_top.sv
@@ -0,0 +1,29 @@
+`include uvm_AFIFO_interface.sv
+`include //design package
+`include //design file
+
+module uvm_AFIFO_top;
+	import uvm_pkg::*;
+
+	uvm_AFIFO_interface vif();
+	
+	FIFO #(DSIZE, ASIZE) fifo (vif.wr_data, vif.wr_clk, vif.wr_rst, vif.wr_inc,
+        vif.rd_clk, vif.rd_rst, vif.rd_inc, vif.rd_data, vif.rd_empty, vif.wr_full);
+
+
+	initial begin
+		uvm_resource_db#(virtual AFIFO_if)::set(.scope("ifs"), .name("AFIO_if"), .val(vif));
+
+		run_test("uvm_AFIFO_test");
+	end
+
+//Variable initialization
+	initial begin
+		vif.wr_clk <= 0;
+		vif.rd_clk <= 0;
+	end
+
+	always
+		#5 vif.wr_clk <= ~vif.wr_clk;
+		#10 vif.rd_clk <= ~vif.rd_clk;
+	endmodule
