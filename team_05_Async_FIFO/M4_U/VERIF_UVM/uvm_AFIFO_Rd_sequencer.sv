2c4e2407a52ec446e201ed2fb17d957901c6077b
Satyajit Deokar <satyajit@mo.ece.pdx.edu>
2025-02-28 14:18:42 -0800

    Adding updated M4

---
+++ team_05_Async_FIFO/M4_U/VERIF_UVM/uvm_AFIFO_Rd_sequencer.sv
@@ -0,0 +1,11 @@
+
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+
+//`include "uvm_AFIFO_agent_pkg.sv"
+import uvm_AFIFO_agent_pkg::*;
+
+
+
+typedef uvm_sequencer#(uvm_AFIFO_Rd_sequence_item) read_sequencer;
+
